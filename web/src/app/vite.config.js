235df13d0d84dfd8c0caab7af71c4a3965aaacf0
Hugo-Olivier Monette <hugoo.monettev4si972@gmail.com>
2022-01-23 15:17:49 -0500

    Working Vite HMR Setup (Dev)

--- web/src/app/vite.config.js
+++ web/src/app/vite.config.js
@@ -1,7 +1,15 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()]
-})
+  plugins: [react()],
+  base: "/dev/",
+  server: {
+    port: 3000,
+    host: true,
+    hmr: {
+      port: 443, //WTF? Internal 443 because of wss? Port's not even exposed ...
+    },
+  },
+});
